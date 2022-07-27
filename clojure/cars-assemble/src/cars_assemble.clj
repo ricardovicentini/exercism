84515aee46ab3ed69e3c769ab529f11e1faa3b68
Ricardo Vicentini <ricardo.vicentini@ricardo.vicentini>
2022-07-27 15:46:19 -0300

    cars-assembly exercise

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,24 +1,22 @@
 (ns cars-assemble)
 
-(def capacity 221)
-(def capacity-90 (* capacity 0.9M))
-(def capacity-80 (* capacity 0.9))
-(def capacity-77 (* capacity 0.7))
+(defn success-factor
+  [speed]
+  (cond (<= speed 0) 0.0
+        (<= speed 4) 1.0
+        (<= speed 8) 0.9
+        (<= speed 9) 0.8
+        (<= speed 10) 0.77
+        :else 0.0))
 
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  (cond
-    (= speed 0) 0
-    (<= speed 4) (* speed capacity)
-    (<= speed 8) (* speed capacity-90)
-    (<= speed 9) (* speed capacity-80)
-    (<= speed 10) (* speed capacity-77)
-    :else -1))
+  (* speed 221 (success-factor speed)))
+
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
-  (let [minutes (* speed 60)]
-    (/ (production-rate speed) 60)))
+  (int (/ (production-rate speed) 60)))
