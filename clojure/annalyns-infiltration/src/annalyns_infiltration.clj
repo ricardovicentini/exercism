8edb7c0a2f40363717aebcc7183ecf63b7bfe033
Ricardo Vicentini <ricardo.vicentini@ricardo.vicentini>
2022-07-31 07:45:43 -0300

    annalyns implementation

---
+++ clojure/annalyns-infiltration/src/annalyns_infiltration.clj
@@ -0,0 +1,24 @@
+(ns annalyns-infiltration)
+
+(defn can-fast-attack?
+  "Returns true if a fast-attack can be made, false otherwise."
+  [knight-awake?]
+  (if (not knight-awake?) true false))
+
+(defn can-spy?
+  "Returns true if the kidnappers can be spied upon, false otherwise."
+  [knight-awake? archer-awake? prisoner-awake?]
+  (or knight-awake? archer-awake? prisoner-awake?))
+
+(defn can-signal-prisoner?
+  "Returns true if the prisoner can be signalled, false otherwise."
+  [archer-awake? prisoner-awake?]
+  (if (and (not archer-awake?) prisoner-awake?) true false))
+
+(defn can-free-prisoner?
+  "Returns true if prisoner can be freed, false otherwise."
+  [knight-awake? archer-awake? prisoner-awake? dog-present?]
+  (cond 
+    (true? dog-present?) (if  (not archer-awake?)  true false)
+    (not dog-present?) (if (and prisoner-awake? (not knight-awake?) (not archer-awake?)) true false)))
+
